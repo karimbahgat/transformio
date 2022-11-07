c341f87e4c3ec0ee60786e9e08b2a19e7b635b3d
Karim Bahgat <karim.bahgat.norway@gmail.com>
2022-11-07 15:38:44 +0100

    Fix requirements file build bug

--- setup.py
+++ setup.py
@@ -1,10 +1,6 @@
 try: from setuptools import setup
 except: from distutils.core import setup
 
-requirements = [line.replace('\n','') for line in open('requirements.txt','r')]
-requirements = [line for line in requirements
-				if line and not line.startswith('#')]
-
 setup(	long_description="Transformio creates, applies, and evaluates coordinate transformations for vector and raster geospatial data.", #open("README.rst").read(), 
 	name="""transformio""",
 	license="""MIT""",
@@ -13,7 +9,9 @@ setup(	long_description="Transformio creates, applies, and evaluates coordinate 
 	version="""0.1.0""",
 	keywords="""transformio""",
 	packages=['transformio'],
-    install_requires=requirements,
+    install_requires=[
+		'numpy==1.*'
+	],
 	classifiers=['License :: OSI Approved', 'Programming Language :: Python', 'Development Status :: 4 - Beta', 'Intended Audience :: Developers', 'Intended Audience :: Science/Research', 'Intended Audience :: End Users/Desktop'],
 	description="""Transformio creates, applies, and evaluates coordinate transformations for vector and raster geospatial data.""",
 	)
